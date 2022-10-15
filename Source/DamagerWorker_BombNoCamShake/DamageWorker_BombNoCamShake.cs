6629d60dc963f43ec1ee44bbcafa8b441bebc070
Mlie <emipa606@users.noreply.github.com>
2022-10-15 22:35:44 +0200

    Mod updated for 1.4 and passed autotests

--- Source/DamagerWorker_BombNoCamShake/DamageWorker_BombNoCamShake.cs
+++ Source/DamagerWorker_BombNoCamShake/DamageWorker_BombNoCamShake.cs
@@ -1,22 +1,19 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using RimWorld;
 using Verse;
 
-namespace Firecracker
-{
-    // Token: 0x02000002 RID: 2
-    public class DamageWorker_BombNoCamShake : DamageWorker_AddInjury
-    {
-        // Token: 0x06000001 RID: 1 RVA: 0x00002050 File Offset: 0x00000250
-        public override void ExplosionStart(Explosion explosion, List<IntVec3> cellsToAffect)
-        {
-            if (def.explosionHeatEnergyPerCell > 1.401298E-45f)
-            {
-                GenTemperature.PushHeat(explosion.Position, explosion.Map,
-                    def.explosionHeatEnergyPerCell * cellsToAffect.Count);
-            }
+namespace Firecracker;
 
-            FleckMaker.Static(explosion.Position, explosion.Map, FleckDefOf.ExplosionFlash, explosion.radius * 6f);
+public class DamageWorker_BombNoCamShake : DamageWorker_AddInjury
+{
+    public override void ExplosionStart(Explosion explosion, List<IntVec3> cellsToAffect)
+    {
+        if (def.explosionHeatEnergyPerCell > 1.401298E-45f)
+        {
+            GenTemperature.PushHeat(explosion.Position, explosion.Map,
+                def.explosionHeatEnergyPerCell * cellsToAffect.Count);
         }
+
+        FleckMaker.Static(explosion.Position, explosion.Map, FleckDefOf.ExplosionFlash, explosion.radius * 6f);
     }
 }
